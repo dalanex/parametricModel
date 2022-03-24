39da9a4f5c0fc5a3d0657ea74694b93015ef5f83
DALANEX <dalanex@inf.upv.es>
2022-03-24 20:01:25 +0100

    comprobacion de numeros solo en las entradas

--- parametricModelApp/src/App.js
+++ parametricModelApp/src/App.js
@@ -8,14 +8,27 @@ function App() {
         <div className="formDiv">
          <form>
            <label className="labels">WIDTH: </label>
-           <input className="inputs" type="text"/> mm<br/>
+           <input className="inputs" type="text" id="inputWidth"/> mm<br/>
            <label className="labels">HEIGHT: </label>
-           <input className="inputs" type="text"/> mm<br/>
+           <input className="inputs" type="text" id="inputHeight"/> mm<br/>
            <label className="labels">DEPTH: </label>
-           <input className="inputs" type="text"/> mm<br/>
+           <input className="inputs" type="text" id="inputDepth"/> mm<br/>
            <label className="labels">LEVELS: </label>
-           <input className="inputs" type="text"/> mm<br/>
-           <button className="submit" onClick= {function() { console.log("SUBMIT"); }}>Submit</button>
+           <input className="inputs" type="text" id="inputLevels"/> mm<br/>
+           <button className="submit" onClick= {function() {
+             var width = document.getElementById('inputWidth').value;
+             var height = document.getElementById('inputHeight').value;
+             var depth = document.getElementById('inputDepth').value;
+             var levels = document.getElementById('inputLevels').value;
+             
+             //alert("Width: " + width + "\nHeight: " + height + "\nDepth: " + depth + "\nLevels: " + levels);
+             
+             var numbers = /^[-+]?[0-9]+$/;
+             if(width.match(numbers) && height.match(numbers) && depth.match(numbers) && levels.match(numbers))
+             {
+                //solo numeros enteros
+             } else { alert("Please, introduce only numbers and without comma"); }
+           }}>Submit</button>
           </form>
         </div>
       </header>
