5183b1e2cae121cea677dd322688b330a4cb5109
SimonBC3 <sbrotons3@gmail.com>
2022-08-06 12:19:37 +0200

    final

---
+++ exec.bat
@@ -0,0 +1,4 @@
+@echo servidor lanzado cierre el terminal para parar la ejecución.
+@echo necesita tener descargado chrome web server porque el navegador no deja acceder a archivos locales.
+@echo se muestra la ejecucion en el video.
+node js/principal.js	
